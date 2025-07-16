49e0018f3b86fcf6e83c991d8892d4939f33b69d
anilkryadav94 <ayadav232@gmail.com>
2025-07-16 17:21:30 +0530

    Update tsdr_app.py

--- tsdr_app.py
+++ tsdr_app.py
@@ -4,7 +4,14 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 from io import BytesIO
 
+# 🚧 Maintenance mode switch
+MAINTENANCE_MODE = True
 
+if MAINTENANCE_MODE:
+    st.set_page_config(layout="wide")
+    st.warning("🛠 App is under maintenance. Please check back later.")
+    st.stop()
+    
 st.set_page_config(layout="wide")
 
 
